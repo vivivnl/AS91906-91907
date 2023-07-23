3d637662f0b04ae86946783b1ef2def95dcbf9ef
Vivian Liu <vivianliu@Vivians-MacBook-Air.local>
2023-07-24 10:46:56 +1200

    Added the title label and made the image background

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from tkinter import *
 import tkinter as tk
 
+
 #create object
-master = tk.Tk()
+root = Tk()
+root.title('Guess the Captial')
+root.geometry("1000x1000")
 
+#define Image
+bg = PhotoImage(file="image.png")
 
+# create a label
+my_label = Label(root, image=bg)
+my_label.place(x=0, y=4, relwidth=1, relheight=1)
 
+#Add something to the top of our image
+my_text = Label(root, text="Guess The Captial", font=("Helvetica", 50), fg="white", bg='#21243B')
+my_text.pack(pady=50)
 
-#add image file
-bgimg = tk.PhotoImage(file= "image.png")
-
-
-limg= Label(master, i=bgimg)
-limg.pack()
 
 #execute tkinter
-master.mainloop()
+root.mainloop()
 
